0d7c9f7902403cc37aa699d8becec4ef98b5a2f7
Leonid ocampo <lnckocampo@yahoo.com>
2023-03-30 17:09:36 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,13 +3,15 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Navbar } from "./navbar.jsx";
+import { Jumbotron } from "./jumbotron.jsx"
 
 //create your first component
 const Home = () => {
 	return (
-		<>
+		<div class = "container">
 			<Navbar/>
-		</>
+			<Jumbotron/>
+		</div>
 	);
 };
 
